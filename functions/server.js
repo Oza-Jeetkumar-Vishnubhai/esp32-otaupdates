1d22e531a7966ea93d39a93e96fd5210debc19c2
Oza Jeetkumar Vishnubhai <onlyjeet31@gmail.com>
2025-01-01 21:46:56 +0530

    first commit

---
+++ functions/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+const PORT = 8000;
+
+// Middleware to parse JSON body
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+  res.status(200).json({es:0});
+})
+
+// Endpoint to check updates
+app.get('/checkUpdates/:version', (req, res) => {
+  const { version } = req.params;
+  console.log(version)
+
+  if (!version) {
+    return res.status(400).json({ error: 'Version is required in the request body.' });
+  }
+
+  const publicDir = path.join(__dirname, 'public');
+
+  // Read all files in the public folder
+  fs.readdir(publicDir, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error reading files from the public folder.' });
+    }
+
+    // Find a file that matches the version
+    console.log(files)
+    if (files[0].split(".ino")[0]!==version) {
+      // Serve the matching file
+      res.setHeader('Content-Type', 'application/octet-stream');
+      res.status(200); 
+      res.sendFile(path.join(publicDir, files[0]));
+    } else {
+      res.status(200).json({update:false});
+    }
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
